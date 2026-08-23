ede4ce52846fd331d55473146cee55d441383755
Anju <anju.sharma045@gmail.com>
2026-08-23 22:38:22 +0930

    updated banners

--- app/api/google-reviews/route.ts
+++ app/api/google-reviews/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY;
   const placeId = process.env.NEXT_PUBLIC_GOOGLE_PLACE_ID;
-  console.log("API Key:", apiKey, "Place ID:", placeId);
 
   const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=name,rating,reviews,user_ratings_total&key=${apiKey}`;
 
